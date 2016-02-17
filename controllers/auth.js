88ff42b8ea5050ae6d6c9c459fdf20da5b8d9262
Scott <scott.heron@outlook.com>
2016-02-17 14:09:04 -0800

    added user sign up functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var db = require('../models');
 var router = express.Router();
 
 router.get('/signup', function(req, res) {
@@ -6,7 +7,26 @@ router.get('/signup', function(req, res) {
 });
 
 router.post('/signup', function(req, res) {
-  res.send(req.body);
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  db.user.findOrCreate({
+  	where: {
+  		email: email
+  	},
+  	defaults: {
+  		name: name,
+  		password: password
+  	}
+  }).spread(function(user, created){
+  	if(created){
+  		res.redirect('/');
+  	} else {
+  		res.send('User already exists');
+  	}
+  }).catch(function(err){
+  	res.send(err);
+  });
 });
 
 router.get('/login', function(req, res) {
