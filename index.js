adb49d4d39975559eef6ee36792fd04f945380cc
Scott <scott.heron@outlook.com>
2016-02-17 15:02:57 -0800

    setup session

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var session = require('express-session');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(ejsLayouts);
+app.use(session({
+	secret: 'dkjfvbngnejknfvdklgehslfnvlusdjnvjsdhcms',
+	resave: false,
+	saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
+  console.log(req.session.animal);
   res.render('index');
+
 });
 
 app.get('/secret', function(req, res) {
+  req.session.animal = 'dog';
   res.render('secret');
 });
 
